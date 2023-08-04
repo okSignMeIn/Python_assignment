7cccfcd7fc7cde5d3d3f9d831ded4ce45662beed
Lakshya <lakshyagupta87@yahoo.com>
2023-08-04 13:08:45 +0530

    Fixed ans2

--- 2/ans.py
+++ 2/ans.py
@@ -3,19 +3,15 @@ import shutil
 
 path = "/home/shtlp_0039/Python_assignment"
 file_and_dirs = os.listdir(path)
-# print(file_and_dirs)
 files = [f for f in file_and_dirs if os.path.isfile(f)]
-# print(files)
 dict={}
 extensions = [f.split('.')[1] for f in files]
-# print(extensions)
 for ext in extensions:
     if os.path.exists(path+"/%s" %ext):
         pass
     else:
         os.mkdir(path+"/%s" %ext)
 for f,e in zip(files,extensions):
-    print(f)
     if e in f:
         if os.path.exists(path+"/%s" %(e+"/"+f)):
             pass
